45e7bede4b3c85527c385a8e2c018e4ec14970a4
korrector <korrector666@gmail.com>
2020-04-09 14:42:00 +0300

    drop down and text field  done

--- src/blocks/_text_field/_text_field.js
+++ src/blocks/_text_field/_text_field.js
@@ -12,31 +12,72 @@ window.addEventListener('DOMContentLoaded', function () {
 		dropInputs = document.querySelectorAll('._text_field__overlay');
 
 
+	// обработка значений при изменении кнопок
+
+	function _summ(_values, summ, _input, valueNames) {
+		_input.value=''; 
+		
+		_values.forEach(function (e,index) {
+			if (valueNames !== undefined ) {
+				if (+e.textContent !==0) {
+					_input.value += `${+e.textContent} ${valueNames[index].textContent} `
+				}
+			} else {
+				summ += +e.textContent;
+				if (summ !== 0) {
+					_input.value = summ + ' гостей';
+				}
+				else {
+					_input.value = '';
+				}
+			}
+		});
+		if (_input.value =='' && valueNames !== undefined) {
+			_input.value = 'ничего не выбрано'
+		}
+		return summ;
+	}
+
 	// навашиваем обработчик на дропы
 	dropInputs.forEach (function (elem) {
 		let btnPlus = elem.querySelectorAll('[data-plus]'),
 			btnMinus = elem.querySelectorAll('[data-minus]'),
 			btnCancel = elem.querySelector('.btn--cancel'),
+			btnSubmit = elem.querySelectorAll('.btn')[1],
 			_input = elem.closest('._text_field').querySelector('input'),
 			_values = elem.closest('._text_field').querySelectorAll('._text_field__value'),
-			summ = _summ(_values, 0, _input);
+			_placeHolder = _input.placeholder, 
+			_spans = elem.querySelectorAll('span'),
+			summ = 0;
+			
+			
+			if (_placeHolder !== '' ) {
+				_spans = undefined;
+
+			} 
+			
+			cl(btnSubmit);
+			summ = _summ(_values, summ, _input, _spans) ;
 
 			//  itit of original state
-			if (summ > 0) {
-				btnCancel.style.display = 'inline-block';
-			}
+			if (btnCancel) {
+				if (summ > 0) {
+					btnCancel.style.display = 'inline-block';
+				}
 
-			if (summ == 0 && btnCancel) {
-				btnCancel.style.display = 'none';
-				(btnCancel.closest('._text_field__overlayItems').classList.add('_text_field__overlayItems--left'));
+				if (summ == 0 && btnCancel) {
+					btnCancel.style.display = 'none';
+					(btnCancel.closest('._text_field__overlayItems').classList.add('_text_field__overlayItems--left'));
+				}	
 			}	
 
 			btnMinus.forEach( (item)=>{
 					let _next = item.nextElementSibling,
-						value = +_next.textContent;
-
+						value = _next.textContent;
+					
 					if (value == 0) {
 						item.classList.add('_text_field__Btn--disabled')
+						{_next.textContent = '0'}
 					}
 			});
 			// следим за всеми дропами, если на них кликаю проверяем на какую кнопку
@@ -54,8 +95,9 @@ window.addEventListener('DOMContentLoaded', function () {
 							_prev.previousElementSibling.classList.remove('_text_field__Btn--disabled')
 						}
 
-						summ = _summ(_values, 0, _input);
-						if (summ > 0) {
+						summ = _summ(_values, 0, _input, _spans);
+
+						if (summ > 0 && btnCancel ) {
 							btnCancel.style.display = 'inline-block';
 							((btnCancel.closest('._text_field__overlayItems').classList.remove('_text_field__overlayItems--left')));
 						}
@@ -63,7 +105,7 @@ window.addEventListener('DOMContentLoaded', function () {
 					}
 				})
 
-				btnMinus.forEach( (item,i)=>{
+				btnMinus.forEach( (item)=>{
 					if (_target == item) {
 						let _next = item.nextElementSibling,
 							value = +_next.textContent,
@@ -77,14 +119,27 @@ window.addEventListener('DOMContentLoaded', function () {
 							item.classList.add('_text_field__Btn--disabled')
 						}
 
-						summ = _summ(_values, 0, _input);
+						summ = _summ(_values, 0, _input, _spans);
 
-						if (summ == 0) {
+						if (summ == 0 && btnCancel ) {
 							btnCancel.style.display = 'none';
+							(btnCancel.closest('._text_field__overlayItems').classList.add('_text_field__overlayItems--left'));
 						}		
-			
 					}
 				})
+
+				if (_target == btnCancel) {
+					_values.forEach( (e) => {
+						e.innerHTML="0";
+						summ = _summ(_values, 0, _input, _spans);
+					}) 
+				}
+
+				if (_target == btnSubmit) {
+					cl(this.parentElement)
+					this.parentElement.classList.remove('_text_field--showDrop');
+					this.parentElement.querySelector('input').classList.remove('_text_field__input--cutBorderBottom')
+				}
 
 
 		})
@@ -100,6 +155,7 @@ window.addEventListener('DOMContentLoaded', function () {
 			_nextSibling.classList.toggle('_text_field__input--cutBorderBottom');
 		});
 	})
+
 	//  скрытие всех открытых вкладок
 	function _clear(i) {
 		for (let elem of arrowsOld){
@@ -114,20 +170,8 @@ window.addEventListener('DOMContentLoaded', function () {
 	}
 	
 	
+
 })
 
-	// обработка значений при изменении кнопок
 
-function _summ(_values, summ, _input) {
-	_values.forEach(function (e) {
-		summ += +e.textContent;
-	});
-	if (summ !== 0) {
-		_input.value = summ + ' гостей';
-	}
-	else {
-		_input.value = '';
-	}
-	return summ;
-}
 
