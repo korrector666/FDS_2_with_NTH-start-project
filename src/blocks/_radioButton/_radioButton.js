2b25d79f9a61e96adf31ee19bc71709426afc83f
korrector <korrector666@gmail.com>
2020-04-11 23:19:44 +0300

    added toggle

--- src/blocks/_radioButton/_radioButton.js
+++ src/blocks/_radioButton/_radioButton.js
@@ -1,7 +1,36 @@
-/* global document */
+/* global document window console */
 
 // const ready = require('../../js/utils/documentReady.js');
+const cl = function (obj) {
+	console.log(obj);
+}
+window.addEventListener('DOMContentLoaded', function () {
 
-// ready(function(){
-//   
-// });
+	let radioButton = document.querySelectorAll('input[type=radio]');
+		
+	radioButton.forEach(function (elem,i,_all) {
+		let _span = elem.nextElementSibling.querySelector('._radioButton__span');
+	
+		_span.classList.remove('_radioButton__span--darkText')
+
+		if (elem.checked == true) {
+			_all.forEach( function(item) {
+				item.nextElementSibling.querySelector('._radioButton__span').classList.remove('_radioButton__span--darkText');
+			});
+			_span.classList.add('_radioButton__span--darkText')
+		}
+
+
+		elem.addEventListener('click', function(e) {
+			let _span = elem.nextElementSibling.querySelector('._radioButton__span');
+
+			if (elem.checked == true) {
+				_all.forEach( function(item) {
+					item.nextElementSibling.querySelector('._radioButton__span').classList.remove('_radioButton__span--darkText');
+				});
+				_span.classList.add('_radioButton__span--darkText')
+			}
+			});
+	})
+	
+});
